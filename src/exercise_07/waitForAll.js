b4ea77c462b63a6333a124cd7779053873e11c6a
yanyuxin <617702281@qq.com>
2020-09-10 10:30:01 +0800

    exercise-7 finish

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -8,6 +8,7 @@ export default function waitForAll(...promises) {
   //
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (promises.some(itm => !(itm instanceof Promise))) {
+    throw new Error('Not all elements are promises.')
+  }
 }
