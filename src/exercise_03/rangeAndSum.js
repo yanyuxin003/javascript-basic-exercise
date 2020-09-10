aaefd96dcf13de8d9c5dced4844d25a544d7c62e
yanyuxin <617702281@qq.com>
2020-09-10 09:38:01 +0800

    exercise-3 finish

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,18 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const result = [];
+  if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      result.push(i);
+    }
+  }
+  if (start > end) {
+    for (let i = start; i > end; i -= 1) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 export function sum(...numbers) {
@@ -17,6 +27,9 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let getSum = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    getSum += numbers[i];
+  }
+  return getSum;
 }
