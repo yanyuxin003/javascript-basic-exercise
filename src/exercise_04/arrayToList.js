9b78364887ac18a24dae11f574d86b274f71fbb0
yanyuxin <617702281@qq.com>
2020-09-10 09:53:51 +0800

    exercise-4 finish

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,16 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let list = null;
+  if (array === null || array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  // 将输出1 2 3 element 写在一起
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    list = { value: array[i], next: list };
+  }
+  return list;
 }
